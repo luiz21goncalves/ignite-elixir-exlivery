0e1801cd78e4a40d2ecd00ce2e4fcda400ed5b3b
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-09-25 20:38:07 -0300

    chore: add address field in user struct

--- lib/users/user.ex
+++ lib/users/user.ex
@@ -1,18 +1,19 @@
 defmodule Exlivery.Users.User do
-  @keys [:name, :email, :document, :age]
+  @keys [:name, :email, :document, :age, :address]
   @enforce_keys @keys
 
   defstruct @keys
 
-  def build(name, email, document, age) when age >= 18 and is_bitstring(document) do
+  def build(name, email, document, age, address) when age >= 18 and is_bitstring(document) do
     {:ok,
      %__MODULE__{
        name: name,
        email: email,
        document: document,
-       age: age
+       age: age,
+       address: address
      }}
   end
 
-  def build(_name, _email, _document, _age), do: {:error, "Invalid parameters"}
+  def build(_name, _email, _document, _age, _address), do: {:error, "Invalid parameters"}
 end
