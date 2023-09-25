8f316722658b08ff7f97d2199ffe5fd5390202ba
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-09-25 20:33:43 -0300

    feat: add unit price validation in item struct

--- lib/orders/item.ex
+++ lib/orders/item.ex
@@ -9,16 +9,26 @@ defmodule Exlivery.Orders.Item do
 
   def build(description, category, unit_price, quantity)
       when quantity > 0 and category in @categories do
+    unit_price
+    |> Decimal.cast()
+    |> build_item(description, category, quantity)
+  end
+
+  def build(_description, _category, _unit_price, _quantity) do
+    {:error, "Invalid parameters"}
+  end
+
+  defp build_item({:ok, price}, description, category, quantity) do
     {:ok,
      %__MODULE__{
        description: description,
        category: category,
-       unit_price: unit_price,
+       unit_price: price,
        quantity: quantity
      }}
   end
 
-  def build(_description, _category, _unit_price, _quantity) do
+  defp build_item(:error, _description, _category, _quantity) do
     {:error, "Invalid parameters"}
   end
 end
