fac0b1f71567159e7d6024fbdc0433a2bda6549f
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-09-25 20:59:20 -0300

    test: user module

---
+++ test/users/user_test.exs
@@ -0,0 +1,45 @@
+defmodule Exlivery.Users.UserTest do
+  use ExUnit.Case
+
+  alias Exlivery.Users.User
+
+  describe "build/5" do
+    test "when all params are valid, returns the user" do
+      response =
+        User.build(
+          "John Doe",
+          "john.doe@email.com",
+          "12345678910",
+          30,
+          "Rua Qualquer"
+        )
+
+      expected_response =
+        {:ok,
+         %User{
+           name: "John Doe",
+           email: "john.doe@email.com",
+           document: "12345678910",
+           age: 30,
+           address: "Rua Qualquer"
+         }}
+
+      assert response == expected_response
+    end
+
+    test "when there are invalid params, returns an error" do
+      response =
+        User.build(
+          "John Doe",
+          "john.doe@email.com",
+          "12345678910",
+          10,
+          "Rua Qualquer"
+        )
+
+      expected_response = {:error, "Invalid parameters"}
+
+      assert response == expected_response
+    end
+  end
+end
