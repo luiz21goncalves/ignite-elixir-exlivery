61c9ffcd98cb8dc1e0403c5fc2cd5be6396d0e77
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-09-25 20:26:53 -0300

    chore: add decimal dependency

--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule Exlivery.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:decimal, "~> 2.1.1"}
     ]
   end
 end
