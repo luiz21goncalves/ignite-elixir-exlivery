68fe84f0b3879dc39e028405a3991afa94509e81
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-09-25 20:07:56 -0300

    feat: add struct validations

--- lib/users/user.ex
+++ lib/users/user.ex
@@ -1,3 +1,18 @@
 defmodule Exlivery.Users.User do
-  defstruct [:name, :email, :document, :age]
+  @keys [:name, :email, :document, :age]
+  @enforce_keys @keys
+
+  defstruct @keys
+
+  def build(name, email, document, age) when age >= 18 and is_bitstring(document) do
+    {:ok,
+     %__MODULE__{
+       name: name,
+       email: email,
+       document: document,
+       age: age
+     }}
+  end
+
+  def build(_name, _email, _document, _age), do: {:error, "Invalid parameters"}
 end
