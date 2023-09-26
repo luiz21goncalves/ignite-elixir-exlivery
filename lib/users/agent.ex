3b111b861cbc0d26ccb237289f77a47215b4e36f
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-09-26 20:37:29 -0300

    feat: add user agent

---
+++ lib/users/agent.ex
@@ -0,0 +1,22 @@
+defmodule Exlivery.Users.Agent do
+  use Agent
+
+  alias Exlivery.Users.User
+
+  def start_link(_initial_state) do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
+
+  def save(%User{} = user), do: Agent.update(__MODULE__, &update_state(&1, user))
+
+  def get(%User{document: document}), do: Access.get(__MODULE__, &get_user(&1, document))
+
+  defp get_user(state, document) do
+    case Map.get(state, document) do
+      nil -> {:error, "User not found"}
+      user -> {:ok, user}
+    end
+  end
+
+  defp update_state(state, %User{document: document} = user), do: Map.put(state, document, user)
+end
