1da647b2f6a6412d69103b2eff0ed109d121b9af
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-09-26 07:43:59 -0300

    test: item struct

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,6 +1,7 @@
 defmodule Exlivery.Factory do
   use ExMachina
 
+  alias Exlivery.Orders.Item
   alias Exlivery.Users.User
 
   def user_factory do
@@ -10,6 +11,15 @@ defmodule Exlivery.Factory do
       document: "12345678910",
       age: 30,
       address: "Rua Qualquer"
+    }
+  end
+
+  def item_factory do
+    %Item{
+      description: "Pizza de peperoni",
+      category: :pizza,
+      quantity: 1,
+      unit_price: Decimal.new("35.5")
     }
   end
 end
